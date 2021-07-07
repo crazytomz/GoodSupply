ea3269e614c7fbf6781905312622cb26e52762f9
thomsonkvarkey@outlook.com <thomsonkvarkey@outlook.com>
2021-07-08 00:10:04 +0530

    Added template

---
+++ GoodSupply.DataTransferObject/DTO/Order.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodSupply.DataTransferObject.DTO
+{
+    public class Order
+    {
+        public int ID { get; set; }
+        public string OrderNumber { get;set; }
+        public string OrderDate { get; set; }
+        [ForeignKey("Supplier")]
+        public int SupplierID { get; set; }
+        public Supplier Supplier { get; set; }
+    }
+    public class OrderDetail
+    {
+        public int ID { get; set; }        
+        public string ItemName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Rate { get; set; }
+        [ForeignKey("Order")]
+        public int OrderID { get; set; }
+        public Order Order { get; set; }
+    }
+}
---
+++ GoodSupply.PGRepository/Services/DataFactory.cs
@@ -0,0 +1,35 @@
+﻿using GoodSupply.DataTransferObject.DTO;
+using GoodSupply.PGRepository.Contracts;
+using Microsoft.Extensions.Options;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodSupply.PGRepository.Services
+{
+    public class DataFactory : IDataFactory
+    {
+        private readonly PGConnectionProperty _pGConnectionProperty;
+        public DataFactory(IOptionsSnapshot<PGConnectionProperty> optionsSnapshot)
+        {
+            _pGConnectionProperty = optionsSnapshot.Value;
+        }
+        private NpgsqlConnection pgSQLCon = null;
+        public NpgsqlConnection GetDBConnection()
+        {
+            try
+            {
+                pgSQLCon = new NpgsqlConnection(_pGConnectionProperty.PGConnectionString);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return pgSQLCon;
+        }
+
+    }
+}
